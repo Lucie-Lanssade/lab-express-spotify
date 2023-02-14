3d71117d970f3e3352ddb5897a34496cd879eb68
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-02-14 08:59:27 +0100

    all iterations, begining css

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -13,6 +14,58 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
-// Our routes go here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch((error) =>
+    console.log('Something went wrong when retrieving an access token', error)
+  );
+
+// Our routes go here:
+app.get('/', async (req, res) => {
+  res.render('home', { title: 'home' });
+});
+
+//iteration 2
+app.get('/artist-search', async (req, res) => {
+  spotifyApi
+    .searchArtists(req.query.name)
+    .then((data) => {
+      res.render('artists', { artists: data.body.artists.items });
+    })
+    .catch((err) =>
+      console.log('The error while searching artists occurred: ', err)
+    );
+});
+
+//iteration 3
+app.get('/albums/:artistId', async (req, res) => {
+  await spotifyApi.getArtistAlbums(req.params.artistId).then((data) => {
+    res.render('albums', { albums: data.body.items });
+    // res.send(data.body.items);
+  }),
+    function (err) {
+      console.error(err);
+    };
+});
+
+//iteration 5
+app.get('/album/:albumId/tracks', async (req, res, next) => {
+  try {
+    const data = await spotifyApi.getAlbumTracks(req.params.albumId);
+    // res.send(data);
+    res.render('tracks', { tracks: data.body.items });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(3000, () =>
+  console.log('My Spotify project running on http://localhost:3000 🎧 🥁 🎸 🔊')
+);
